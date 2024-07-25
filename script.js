4dabc780db7d5b6187ecedf5c2150a9a9d4a8f55
Vipul Goel <111168306+VipulGoel08@users.noreply.github.com>
2024-07-25 14:44:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let speech = new SpeechSynthesisUtterance();
+
+const button = document.querySelector("button");
+const textArea = document.querySelector("textarea");
+let voices = [];
+
+let voiceSelect = document.querySelector("select")
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voices = voices[0];
+
+    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
+};
+
+voiceSelect.addEventListener("change", () => {
+    speech.voice = voices[voiceSelect.value];
+});
+
+button.addEventListener("click", ()=>{
+    speech.text = textArea.value;
+    window.speechSynthesis.speak(speech);
+})
